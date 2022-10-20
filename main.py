b537705a3b83bf8e81ae6e9caf19b64c7f46a7a5
Alima <aliali.ali1378@yahoo.com>
2022-10-20 19:01:23 +0330

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
